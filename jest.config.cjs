9d53ec72fde351ccd32e41bf3cf3009d11fcd4cf
thegfmachado <gabriel.ferreira.machado@hotmail.com>
2025-01-01 16:39:28 -0300

    test(typewriter): jest setup and typewriter tests

---
+++ jest.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom', // Use the installed environment explicitly
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest', // Use ts-jest to handle .ts and .tsx files
+  },
+  globals: {
+    'ts-jest': {
+      useESM: true, // Enable ESM support in ts-jest
+    },
+  },
+  moduleNameMapper: {
+    '^react$': 'react', // This is useful if you're mapping React
+  },
+  extensionsToTreatAsEsm: ['.ts', '.tsx', '.jsx'], // Remove .js here
+  testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'], // Match .ts, .tsx, .js, and .jsx files
+};
---
+++ src/use-typewriter.test.tsx
@@ -0,0 +1,154 @@
+import { act, renderHook } from "@testing-library/react";
+import { useTypewriter } from "./use-typewriter";
+
+afterEach(() => {
+  jest.clearAllTimers();
+});
+
+test('should append characters correctly when write is called', async () => {
+  const elem = document.createElement('div');
+  const targetRef = { current: elem };
+
+  const speed = 100;
+  const buffer = 100;
+  const text = 'Hello';
+  const timeToComplete = speed * text.length + buffer;
+
+  const { result } = renderHook(() => useTypewriter({ targetRef, speed }));
+
+  act(() => {
+    result.current.write(text).start();
+  });
+
+  await new Promise((resolve) => setTimeout(resolve, timeToComplete));
+
+  expect(targetRef.current?.textContent).toBe(text);
+});
+
+test('should delete characters correctly when delete is called', async () => {
+  const elem = document.createElement('div');
+  const targetRef = { current: elem };
+
+  const speed = 100;
+  const buffer = 100;
+  const text = 'Hello';
+  const timeToComplete = speed * text.length + buffer;
+
+  const { result } = renderHook(() => useTypewriter({ targetRef, speed }));
+
+  act(() => {
+    result.current.write(text).start();
+  });
+
+  await new Promise((resolve) => setTimeout(resolve, timeToComplete));
+
+  act(() => {
+    result.current.delete(3).start();
+  });
+
+  await new Promise((resolve) => setTimeout(resolve, timeToComplete));
+
+  expect(targetRef.current?.textContent).toBe('He');
+});
+
+test('should call onWrite when new text is written', async () => {
+  const onWriteMock = jest.fn();
+
+  const elem = document.createElement('div');
+  const targetRef = { current: elem };
+
+  const speed = 100;
+  const buffer = 100;
+  const text = 'Hello';
+  const timeToComplete = speed * text.length + buffer;
+
+  const { result } = renderHook(() =>
+    useTypewriter({ targetRef, speed, onWrite: onWriteMock })
+  );
+
+  act(() => {
+    result.current.write(text).start();
+  });
+
+  await new Promise((resolve) => setTimeout(resolve, timeToComplete));
+
+  expect(onWriteMock).toHaveBeenCalledWith('H');
+  expect(onWriteMock).toHaveBeenCalledWith('He');
+  expect(onWriteMock).toHaveBeenCalledWith('Hel');
+  expect(onWriteMock).toHaveBeenCalledWith('Hell');
+  expect(onWriteMock).toHaveBeenCalledWith('Hello');
+});
+
+test('should call onComplete when the writing is done', async () => {
+  const onCompleteMock = jest.fn();
+
+  const elem = document.createElement('div');
+  const targetRef = { current: elem };
+
+  const speed = 100;
+  const buffer = 100;
+  const text = 'Hello';
+  const timeToComplete = speed * text.length + buffer;
+
+  const { result } = renderHook(() =>
+    useTypewriter({ targetRef, speed, onComplete: onCompleteMock })
+  );
+
+  act(() => {
+    result.current.write(text).start();
+  });
+
+  await new Promise((resolve) => setTimeout(resolve, timeToComplete));
+
+  expect(onCompleteMock).toHaveBeenCalledTimes(1);
+});
+
+test('should reset the text when reset is called', async () => {
+  const elem = document.createElement('div');
+  const targetRef = { current: elem };
+
+  const speed = 100;
+  const buffer = 100;
+  const text = 'Hello';
+  const timeToComplete = speed * text.length + buffer;
+
+  const { result } = renderHook(() => useTypewriter({ targetRef, speed }));
+
+  act(() => {
+    result.current.write(text).start();
+  });
+
+  await new Promise((resolve) => setTimeout(resolve, timeToComplete));
+
+  expect(targetRef.current?.textContent).toBe(text);
+
+  act(() => {
+    result.current.reset();
+  });
+
+  expect(targetRef.current?.textContent).toBe('');
+});
+
+test('should handle looping of text', async () => {
+  const onWriteMock = jest.fn();
+
+  const elem = document.createElement('div');
+  const targetRef = { current: elem };
+
+  const speed = 100;
+  const buffer = 100;
+  const text = 'Hello';
+  const timeToComplete = speed * text.length + buffer;
+
+  const { result } = renderHook(() =>
+    useTypewriter({ targetRef, speed, loop: true, onWrite: onWriteMock })
+  );
+
+  act(() => {
+    result.current.write(text).start();
+  });
+
+  await new Promise((resolve) => setTimeout(resolve, timeToComplete * 2));
+
+  expect(onWriteMock).toHaveBeenNthCalledWith(2, 'He');
+});
